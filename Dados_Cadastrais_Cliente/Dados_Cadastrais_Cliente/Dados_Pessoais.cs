0d3703f1eada12c387183b8e00f2e2fd7f8e16b9
Leandro Godoy <Leandro.e.Katia1@gmail.com>
2022-01-26 22:54:10 -0300

    Novo Projeto Cadastro de Cliente

---
+++ Dados_Cadastrais_Cliente/Dados_Cadastrais_Cliente/Dados_Pessoais.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dados_Cadastrais_Cliente
+{
+    class Dados_Pessoais
+    {
+        public string[] Nome = new string[80];
+        public int[] Idade = new int[80];
+        public string[] Cpf = new string[80];
+    }
+}
---
+++ Dados_Cadastrais_Cliente/Dados_Cadastrais_Cliente/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Dados_Cadastrais_Cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dados_Pessoais dados_pessoais = new Dados_Pessoais();
+
+            Console.Write("Gostaria de se cadastrar? digite 's' para sim ou 'n' para não - ");
+            string opcao = Console.ReadLine();
+            Console.WriteLine();
+            int i = 0;
+
+            while (opcao != "s" & opcao != "n")
+            {
+                Console.Write("Digite 's' para sim ou 'n' para não - ");
+                opcao = Console.ReadLine();
+            }
+            Console.WriteLine();
+
+            if (opcao == "n")
+            {
+                Console.WriteLine("Obrigado pela visita!!! ");
+            }
+            else
+            {
+                while (opcao == "s")
+                {
+
+                    while (opcao == "s")
+                    {
+                        Console.WriteLine();
+                        Console.Write("Digite o nome: ");
+                        dados_pessoais.Nome[i] = Console.ReadLine();
+
+                        Console.Write("Digite a idade: ");
+                        dados_pessoais.Idade[i] = int.Parse(Console.ReadLine());
+
+                        Console.Write("Digite o CPF: ");
+                        dados_pessoais.Cpf[i] = Console.ReadLine();
+                        Console.WriteLine();
+
+                        i++;
+
+                        Console.Write("Gostaria de cadastrar outra Pessoa? digite 's' para sim ou 'n' para não - ");
+                        opcao = Console.ReadLine();
+                        Console.WriteLine();
+                    }
+
+                    Console.WriteLine("Confira seus dados: ");
+
+                    int j = 0;
+                    for (j = 0; j < i; j++)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Nome.... " + dados_pessoais.Nome[j]);
+                        Console.WriteLine("Idade... " + dados_pessoais.Idade[j] + " anos");
+                        Console.WriteLine("CPF..... " + dados_pessoais.Cpf[j]);
+
+                    }
+                    Console.WriteLine();
+                    Console.Write ("Estão corretos? digite: 1 para sim ou 2 para não? - ");
+
+                    int escolha = int.Parse(Console.ReadLine());
+
+                    if (escolha == 1)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Cadastro efetuado com sucesso!!!");
+                    }
+                    else
+                    {
+                        Console.Write("Corrigir os erros - digite 's' para sim ou 'n' para não - ");
+                        opcao = Console.ReadLine();
+                        Console.WriteLine();
+                        i = 0;
+                    }
+                }
+
+            }
+
+        }
+    }
+}
